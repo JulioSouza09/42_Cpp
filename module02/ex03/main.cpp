59c3cb7dc79f61dc9d12448e0192d2010503e9ff
Julio Souza <jcesarsss2002@gmail.com>
2026-04-11 18:42:26 +0100

    Remove wrong test case unnecessary comments

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "bsp.hpp"
 
-// Colors for output
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
@@ -25,38 +24,16 @@ void run_test(std::string name, Point const a, Point const b, Point const c, Poi
 
 int main()
 {
-    /* // Define a triangle: A(0,0), B(10,0), C(0,10)
-    Point a(0, 0);
-    Point b(10, 0);
-    Point c(0, 10);
-
-    std::cout << "--- Starting BSP Tests ---" << std::endl;
-
-    // 1. Points clearly inside
-    run_test("Inside Center", a, b, c, Point(2, 2), true);
-    run_test("Inside Near Corner", a, b, c, Point(0.1f, 0.1f), true);
-
-    // 2. Points clearly outside
-    run_test("Outside Far", a, b, c, Point(10, 10), false);
-    run_test("Outside Negative", a, b, c, Point(-1, -1), false);
-    run_test("Outside Near Edge", a, b, c, Point(5, -0.1f), false);
-
-    // 3. Edge cases (Should be false per your logic)
-    run_test("On Vertex A", a, b, c, Point(0, 0), false);
-    run_test("On Edge AB", a, b, c, Point(5, 0), false);
-    run_test("On Edge BC", a, b, c, Point(5, 5), false);
-
-    std::cout << "--- Tests Complete ---" << std::endl; */
-
     Point a(0, 0);
     Point b(1, 2.2f);
     Point c(2.4f, 1);
 
     run_test("Outside  with y = min fixed", a, b, c, Point(0, 0.00390625f), false);
-    run_test("Inside with y and x = min fixed", a, b, c, Point(0.00390625f, 0.00390625f), true);
+    run_test("Inside with y = min fixed", a, b, c, Point(0.5f, 1.00390625f), true);
     run_test("Outsidee with x = min fixed", a, b, c, Point(0.00390625f, 0), false);
     run_test("Clearly inside", a, b, c, Point(1.2f, 1), true);
     run_test("Clearly outside", a, b, c, Point(1.2f, -1), false);
+    run_test("On top of the vertice", a, b, c, Point(1, 2.2f), false);
 
     return 0;
 }
