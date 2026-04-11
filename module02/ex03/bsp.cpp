da51c672552f9ed48e2f58223541b82c47a3503f
Julio Souza <jcesarsss2002@gmail.com>
2026-04-11 11:07:43 +0100

    feat: bsp.cpp - Remove debug prints from triangleArea() - Add debug prints on bsp() - Change validation to accept only points that are strictly inside the triangle

--- module02/ex03/bsp.cpp
+++ module02/ex03/bsp.cpp
@@ -6,9 +6,6 @@ Fixed triangleArea(const Point& a, const Point& b, const Point& c)
     Point ab = b - a;
     Point ac = c - a;
     Fixed cross = ab.cross(ac);
-    std::cout << "ab = " << ab << std::endl;
-    std::cout << "ac = " << ac << std::endl;
-    std::cout << "cross = " << cross << std::endl;
     return Fixed(0.5f) * cross;
 }
 
@@ -16,16 +13,20 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
     Fixed abcArea = triangleArea(a, b, c);
 
+    std::cout << "===== " << point << " =====" << std::endl;
+    std::cout << "abcArea = " << abcArea << std::endl;
     Point pb = b - point;
     Point pc = c - point;
     Point pa = a - point;
-    std::cout << "Point = " << point << std::endl;
+    std::cout << "pa = " << pa << std::endl;
     std::cout << "pb = " << pb << std::endl;
     std::cout << "pc = " << pc << std::endl;
-    std::cout << "pa = " << pa << std::endl;
     Fixed aP = Fixed(0.5f) * (pb.cross(pc)) / abcArea;
     Fixed bP = Fixed(0.5f) * (pc.cross(pa)) / abcArea;
     Fixed cP = Fixed(0.5f) * (pa.cross(pb)) / abcArea;
+    std::cout << "aP = " << aP << std::endl;
+    std::cout << "bP = " << bP << std::endl;
+    std::cout << "cP = " << cP << std::endl;
 
-    return aP >= 0 && aP <= 1 && bP >= 0 && bP <= 1 && cP >= 0 && cP <= 1;
+    return aP > 0 && aP < 1 && bP > 0 && bP < 1 && cP > 0 && cP < 1;
 }
